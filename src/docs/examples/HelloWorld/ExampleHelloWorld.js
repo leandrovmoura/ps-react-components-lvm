d34663f9a7299e1dda3f3cfe07c9fdc4eac6e5d9
Leandro Vieira de Moura <leandrovmoura@gmail.com>
2021-03-18 15:08:01 -0300

    Finished Mod 4

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'ps-react-components-lvm/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+    return <HelloWorld message="Pluralsight viewers!" />
+}
