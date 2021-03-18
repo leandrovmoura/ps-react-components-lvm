03d0d9141d733d3fe618ee3049cb25b5dd9dfb71
Leandro Vieira de Moura <leandrovmoura@gmail.com>
2021-03-18 16:03:18 -0300

    Finished mod 6

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'ps-react-components-lvm/Label';
+
+/** Optional label */
+export default function ExampleOptional() {
+    return <Label htmlFor="test" label="test" />
+}
